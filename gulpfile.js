2153729e8d0ff83b833fa7bcf7a3882084882f90
zainabbas <zainabbaskhakhi123@gmail.com>
2024-12-19 20:21:33 +0500

    add dist directory for production

--- gulpfile.js
+++ gulpfile.js
@@ -21,8 +21,8 @@ var path = {
     images: "source/images/**/*.+(png|jpg|gif|svg)",
   },
   build: {
-    dirBuild: "theme/",
-    dirDev: "theme/",
+    dirBuild: "dist/",
+    dirDev: "dist/",
   },
 };
 
